6965e3fc6f7bf17156af2af8b3baa8f8788034be
Ankit <ankit@ankit.com>
2020-03-21 03:49:21 +0530

    Timeline Added and Table View Updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,77 +1,164 @@
 import React, {Component} from 'react';
 import { Table } from 'reactstrap';
-
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
+import destination from '../img/destination.svg'
+import warehouse from '../img/warehouse.svg'
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
- class Hero extends Component {
-  state = {
-    orders: []
-  }
-
-  componentDidMount() {
-    axios({ method: 'POST', url: 'https://93870v1pgk.execute-api.ap-south-1.amazonaws.com/latest/shipments/mayank', headers: {Authorization: 'Bearer tTU3gFVUdP'}, data: { email: 'mayankmittal@intugine.com' } })
-    .then(res=>{
-            const orders = res.data.data;
-            // console.log(orders[0])
-            this.setState({orders})
-            console.log(this.state.orders)
-
+class Hero extends Component 
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = 
+        {
+            results: [],
+            scan_rev: []   
+        }
            
-  })
-      
-  }
-
-  renderTableData() {
-    return this.state.orders.map((order, index) => {
-       // const { order.extra_fields.expected_delivery_date}=order;
-       const {_id, awbno, carrier, from, to ,pickup_date , extra_fields,current_status} = order
-      
-       return (
+    }
+    
+    fetchDetails = (result) =>
+    {
+     
+      console.log('We need to get the details for ', result);
+      console.log(result.scan)
+      const scan_rev = result.scan;
+      this.setState({scan_rev})
+    
+    }
+    
+    
+    
        
-          <tr key={_id}>
+    
+    
+          componentDidMount()
+          {
+            axios({ method: 'POST', url: 'https://93870v1pgk.execute-api.ap-south-1.amazonaws.com/latest/shipments/mayank', headers: {Authorization: 'Bearer tTU3gFVUdP'}, data: { email: 'mayankmittal@intugine.com' } })
+            .then(res=>{
+                    const results = res.data.data;
+                    const fetch = res.data.data[0].scan;
+                    const scan_rev = fetch.reverse();
+                    this.setState({results})
+                    this.setState({scan_rev})
+        
+                   
+          })
+        }
+    
+        TableData()
+        {
+            return this.state.results.map((result, index) => {
+             
+               const {_id, awbno, carrier, from, to ,pickup_date , extra_fields,current_status} = result
+               return(
+               
+                <tr key={index}  onClick={() => this.fetchDetails(result)} >
+                    <Link to="/" style={{textDecoration:" none"}}>   <td className="td">{awbno}</td>  </Link>
+                     <td className="td">{carrier}</td>
+                     <td className="td">{from}</td>
+                     <td className="td">{to}</td>
+                     <td className="td">{carrier}</td>
+                     <td className="td">{pickup_date}</td>
+                     <td className="td">{extra_fields? extra_fields.expected_delivery_date: ''}</td>
+                     <td className="td" style = {current_status==='Delivered'?{color:'green'}:null}>{current_status}</td>
+                     
+                </tr>
+                 
+               )
+            })
+         }
+        
+        
+    
+    
+    
+     TimeLineData()
+     {
+        return this.state.scan_rev.map((scan, index) =>
+        {
+            const {time, location, status_detail} = scan
+            return(
+                <TimelineItem>
+                    <p className="timeline_content">
+                    <span className="timeline_status">
+                        {status_detail}
+                    </span>
+                    <span className="timeline_date">
+                        {time}
+                    </span>
+                    <span className="timeline_time">
+                        {location}
+                    </span>
+                    </p>
+                </TimelineItem>
+            )
+    
+        }
+        
+        )
+    }
+    
+     
+    
+     
+    
+      render()
+      {
+   
+            const TimelineList = () => {
+            return(
             
-             <td>{awbno}</td>
-             <td>{carrier}</td>
-             <td>{from}</td>
-             <td>{to}</td>
-             <td>{carrier}</td>
-             <td>{pickup_date}</td>
-             <td>{extra_fields? extra_fields.expected_delivery_date: ''}</td>
-             <td>{current_status}</td>
-            
-          </tr>
-       )
-    })
- }
+                <div>
+                <Timeline lineColor={'#ddd'}>
+                    <div className="timeline_image1">
+                        <img src={destination} alt="" />
+                    </div>
+                    {this.TimeLineData()}
 
-  render() {
-    return (
- //     <div>
- //         <ul>
- //             {this.state.orders.map(order => {
- //                 return <li>{order.awbno}</li>
- //             })}
- //         </ul>
- //     </div>
- <Table responsive >
- <thead >
-   <tr>
-     <th>AWB NUMBER</th>
-     <th>TRANSPORTER</th>
-     <th>SOURCE</th>
-     <th>DESTINATION</th>
-     <th>BRAND</th>
-     <th>START DATE</th>
-     <th>ETD</th>
-     <th>STATUS</th>
-   </tr>
- </thead>
- <tbody>
- {this.renderTableData()}
- </tbody>
-</Table>
-            
-    )
-  }
-}
+                    <div className="timeline_image2">
+                        <img src={warehouse} alt="" />
+                    </div>
+                </Timeline> 
+            </div>
+          
+          );
+        }
+    
+    
+    
+        return (
+   
+            <div class="row row-content ">
+                <div class="col-12 col-sm-6">
+                    <TimelineList/>
+                </div>
+                <div class="col-12 col-sm-4 order-sm-last col-md-6">
+                    <Table className="table table-fixed">
+                        <thead >
+                            <tr>
+                                <th className="th">AWB NUMBER</th>
+                                <th className="th">TRANSPORTER</th>
+                                <th className="th">SOURCE</th>
+                                <th className="th">DESTINATION</th>
+                                <th className="th">BRAND</th>
+                                <th className="th">START DATE</th>
+                                <th className="th">ETD</th>
+                                <th className="th">STATUS</th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            {this.TableData()}
+                        </tbody>
+                    </Table>
+        
+                </div>
+    
+        </div>         
+        )
+      }
+    }
+        
 export default Hero;
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,148 +1,154 @@
 import React, { Component, PropTypes } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Hero from './Hero';
-import { Timeline,Icon } from 'rsuite';
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button,CardHeader, CardGroup
-  } from 'reactstrap';
-import ReactDOM from 'react-dom'
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 import axios from 'axios';
-class HeroSection extends Component {
-    
-    state = {
-        orders: []
-      }
 
-      
+class HeroSection extends Component
+{
 
-      componentDidMount()
-      {
-        axios({ method: 'POST', url: 'https://93870v1pgk.execute-api.ap-south-1.amazonaws.com/latest/shipments/mayank', headers: {Authorization: 'Bearer tTU3gFVUdP'}, data: { email: 'mayankmittal@intugine.com' } })
-        .then(res=>{
-                const orders = res.data.data;
-                
-                this.setState({orders})
-                console.log(this.state.orders)
-    
-               
-      })
-          
-      }
-      renderTableData() {
-        return this.state.orders.map((order, index) => {
-           // const { order.extra_fields.expected_delivery_date}=order;
-           const {_id, awbno, carrier, from, to ,pickup_date , extra_fields,current_status} = order
-          
-           return (
-           
-              <tr key={_id}>
-                
-                 <td>{awbno}</td>
-                 <td>{carrier}</td>
-                 <td>{from}</td>
-                 <td>{to}</td>
-                 <td>{carrier}</td>
-                 <td>{pickup_date}</td>
-                 <td>{extra_fields? extra_fields.expected_delivery_date: ''}</td>
-                 <td>{current_status}</td>
-                
-              </tr>
-           )
-        })
-     }
-
-     render(){
-
-        const TimelineList = () => {
-            return(
-                <Timeline>
-                <Timeline.Item dot={<Icon icon="check-circle" style={{ color: 'green' }} />}>
-                  <p>2018-03-01</p>
-                  <p>Your order starts processing</p>
-                </Timeline.Item>
-                <Timeline.Item dot={<Icon icon="exclamation-triangle" style={{ color: 'orange' }} />}>
-                  <p>2018-03-02</p>
-                  <p>Order out of stock</p>
-                </Timeline.Item>
-                <Timeline.Item dot={<Icon icon="info-circle" style={{ color: 'blue' }} />}>
-                  <p>2018-03-10</p>
-                  <p>Arrival</p>
-                </Timeline.Item>
-                <Timeline.Item dot={<Icon icon="check-circle" style={{ color: 'green' }} />}>
-                  <p>2018-03-12</p>
-                  <p>Order out of the library</p>
-                </Timeline.Item>
-                <Timeline.Item dot={<Icon icon="spinner" spin style={{ borderRadius:'50%' }} />}>
-                  <p>2018-03-15</p>
-                  <p>Sending you a piece</p>
-                </Timeline.Item>
-              </Timeline>
-            );
-          }
-
-      
-      
-
-      
-
-        return (
-            <BrowserRouter>
-
-                <div>
-            
-                      
-    
-  
-      
-    <div className="cards">
-          <div className="card">
-            <p className="card_head">DEL</p>
-            <p className="card_no">916</p >
-          </div>
-          <div className="card">
-            <p className="card_head">DEL</p>
-            <p className="card_no">916</p >
-          </div>
-          <div className="card">
-            <p className="card_head">DEL</p>
-            <p className="card_no">916</p >
-          </div>
-          <div className="card">
-            <p className="card_head">DEL</p>
-            <p className="card_no">916</p >
-          </div>
-          <div className="card">
-            <p className="card_head">DEL</p>
-            <p className="card_no">916</p >
-          </div>
-        </div>
-     
-    
-
-
-    <div class="row row-content ">
-       
-    <div class="col-sm-4 col-md-4 order-2">
-        <TimelineList/>
-    </div>
-    <div class="col-12 col-sm-4 order-sm-last col-md-7">
-        <Hero/>
-    </div>
-
-    </div>
-           
-    </div>
-
-              
-
-                   
-
-
-            </BrowserRouter>
-           
-          );
-          }
+    constructor(props)
+    {
+        super(props)
+        this.state =
+        {
+            results: [],
+            scan_rev:[],
+            fetch:[]
         }
+        
+    }
+    
+
+    
+    
+          
+    
+            componentDidMount()
+            {
+                axios({ method: 'POST', url: 'https://93870v1pgk.execute-api.ap-south-1.amazonaws.com/latest/shipments/mayank',
+                headers: {Authorization: 'Bearer tTU3gFVUdP'}, data: { email: 'mayankmittal@intugine.com' } })
+                .then
+                (res=>
+                    {
+                        const results = res.data.data;
+                        fetch = res.data.data[0].scan;
+                        const scan_rev = fetch;
+                        this.setState({results})
+                        this.setState({scan_rev})
+                    }
+                )
+            }
+    
+          renderTableData() 
+          {
+            return this.state.results.data.map((result, index) =>
+            {
+               
+               const {_id, awbno, carrier, from, to ,pickup_date , extra_fields,current_status} = result
+             
+               return (
+               
+                  <tr key={_id}  >
+                    
+                     <td>{awbno}</td>
+                     <td>{carrier}</td>
+                     <td>{from}</td>
+                     <td>{to}</td>
+                     <td>{carrier}</td>
+                     <td>{pickup_date}</td>
+                     <td>{extra_fields? extra_fields.expected_delivery_date: ''}</td>
+                     <td>{current_status}</td>
+                    
+                  </tr>
+               )
+            })
+         }
+    
+         renderTimeLineData(){
+          return this.state.scan_rev.map((scan, index) => {
+            const {time, location, status_detail} = scan
+            return(
+              <TimelineItem>
+              <p className="timeline_content">
+                <span className="timeline_status">
+                  {status_detail}
+              </span>
+                <span className="timeline_date">
+                  {time}
+              </span>
+                <span className="timeline_time">
+                  {location}
+              </span>
+              </p>
+            </TimelineItem>
+            )
+    
+          }
+            
+          )}
+    
+         
+        
+        
+    
+        render()
+        {
+            const a =this.state.results;
+            let DEL =0,INT=0,OOD=0,DEX=0,NFI=0;
+            if(this.state.results)
+            {
+                    a.map(data => data.current_status_code === 'DEL' ? DEL=DEL+1:DEL=DEL)
+            }
+            if(this.state.results)
+            {
+                    a.map(data => data.current_status_code === 'INT' ? INT=INT+1:INT=INT)
+            }
+            if(this.state.results)
+            {
+                    a.map(data => data.current_status_code === 'OOD' ? OOD=OOD+1:OOD=OOD)
+            }
+            if(this.state.results)
+            {
+                    a.map(data => data.current_status_code === 'DEX' ? DEX=DEX+1:DEX=DEX)
+            }
+            if(this.state.results)
+            {
+                    a.map(data => data.current_status_code === 'NFI' ? NFI=NFI+1:NFI=NFI)
+            }
+                return (
+            <BrowserRouter className="">
+             <div className="container ">      
+            <div className="cards mr">
+            <div className="card">
+            <p className="card_head">DEL</p>
+                <p className="card_no">{DEL}</p >
+            </div>
+            <div className="card">
+            <p className="card_head">INT</p>
+                <p className="card_no">{INT}</p >
+            </div>
+            <div className="card">
+            <p className="card_head">OOD</p>
+                <p className="card_no">{OOD}</p >
+            </div>
+            <div className="card">
+            <p className="card_head">DEX</p>
+                <p className="card_no">{DEX}</p >
+            </div>
+            <div className="card">
+            <p className="card_head">NFI</p>
+            <p className="card_no">{NFI}</p >
+            </div>
+            </div>
+            </div>   
+            
+            <Hero/>
+        
+            </BrowserRouter>
+                
+            );
+        }
+}
 
 export default HeroSection;
